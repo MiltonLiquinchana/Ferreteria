451a7e129a2f0dc53ae9859a3a736e25df4b186e
MiltonLiquinchana <liquinchana@hotmail.es>
2020-08-06 21:53:38 -0500

    se creo el metodo agregarlote en la clase ClsCompra el cual recive cuatro parametros(codigo, fecha de caducidad, cantidad y id del producto), todo esto se guarda en la tabla lote

--- src/Negocio/ClsCompra.java
+++ src/Negocio/ClsCompra.java
@@ -15,63 +15,67 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class ClsCompra {
-private Connection connection=new ClsConexion().getConection();
+
+    private Connection connection = new ClsConexion().getConection();
+    /*variable de tipo boleana para saber si se guardo o no los datos*/
+    boolean trueandfalse,trueandfalselote;
 //--------------------------------------------------------------------------------------------------
 //-----------------------------------------METODOS--------------------------------------------------
 //-------------------------------------------------------------------------------------------------- 
-    public void agregarCompra(ClsEntidadCompra Compra){
-        try{
-            CallableStatement statement=connection.prepareCall("{call SP_I_Compra(?,?,?,?,?,?,?,?,?)}");
-            statement.setString("pidtipodocumento",Compra.getStrIdTipoDocumento());
-            statement.setString("pidproveedor",Compra.getStrIdProveedor());
-            statement.setString("pidempleado",Compra.getStrIdEmpleado());
-            statement.setString("pnumero",Compra.getStrNumeroCompra());
-            statement.setDate ("pfecha", new java.sql.Date(Compra.getStrFechaCompra().getTime()));
-            statement.setString("psubtotal",Compra.getStrSubTotalCompra());
-            statement.setString("pigv",Compra.getStrIgvCompra());
-            statement.setString("ptotal",Compra.getStrTotalCompra());
-            statement.setString("pestado",Compra.getStrEstadoCompra());
-            statement.execute();
 
-            JOptionPane.showMessageDialog(null,"¡Compra Realizada con éxito!","Mensaje del Sistema",1);
+    public boolean agregarCompra(ClsEntidadCompra Compra) {
+        try {
+            CallableStatement statement = connection.prepareCall("{call SP_I_Compra(?,?,?,?,?,?,?,?,?)}");
+            statement.setString("pidtipodocumento", Compra.getStrIdTipoDocumento());
+            statement.setString("pidproveedor", Compra.getStrIdProveedor());
+            statement.setString("pidempleado", Compra.getStrIdEmpleado());
+            statement.setString("pnumero", Compra.getStrNumeroCompra());
+            statement.setDate("pfecha", new java.sql.Date(Compra.getStrFechaCompra().getTime()));
+            statement.setString("psubtotal", Compra.getStrSubTotalCompra());
+            statement.setString("pigv", Compra.getStrIgvCompra());
+            statement.setString("ptotal", Compra.getStrTotalCompra());
+            statement.setString("pestado", Compra.getStrEstadoCompra());
+            trueandfalse = statement.execute();
 
-        }catch(SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        
-    }    
-    public void modificarCompra(String codigo,ClsEntidadCompra Compra){
-        try{
-            CallableStatement statement=connection.prepareCall("{call SP_U_Compra(?,?,?,?,?,?,?,?,?,?)}");
-            statement.setString("pidcompra",codigo);
-            statement.setString("pidtipodocumento",Compra.getStrIdTipoDocumento());
-            statement.setString("pidproveedor",Compra.getStrIdProveedor());
-            statement.setString("pidempleado",Compra.getStrIdEmpleado());
-            statement.setString("pnumero",Compra.getStrNumeroCompra());
-            statement.setDate ("pfecha", new java.sql.Date(Compra.getStrFechaCompra().getTime()));
-            statement.setString("psubtotal",Compra.getStrSubTotalCompra());
-            statement.setString("pigv",Compra.getStrIgvCompra());
-            statement.setString("ptotal",Compra.getStrTotalCompra());
-            statement.setString("pestado",Compra.getStrEstadoCompra());
-            statement.executeUpdate();
-            
-        }catch(SQLException ex){
-            ex.printStackTrace();
-        }
-        JOptionPane.showMessageDialog(null,"¡Compra Actualizada!","Mensaje del Sistema",1);
+        return trueandfalse;
     }
-    public ArrayList<ClsEntidadCompra> listarCompra(){
-        ArrayList<ClsEntidadCompra> compras=new ArrayList<ClsEntidadCompra>();
-        try{
-            CallableStatement statement=connection.prepareCall("{call SP_S_Compra}");
-            ResultSet resultSet=statement.executeQuery();
-            
-            while (resultSet.next()){
-                ClsEntidadCompra compra=new ClsEntidadCompra();
+
+    public void modificarCompra(String codigo, ClsEntidadCompra Compra) {
+        try {
+            CallableStatement statement = connection.prepareCall("{call SP_U_Compra(?,?,?,?,?,?,?,?,?,?)}");
+            statement.setString("pidcompra", codigo);
+            statement.setString("pidtipodocumento", Compra.getStrIdTipoDocumento());
+            statement.setString("pidproveedor", Compra.getStrIdProveedor());
+            statement.setString("pidempleado", Compra.getStrIdEmpleado());
+            statement.setString("pnumero", Compra.getStrNumeroCompra());
+            statement.setDate("pfecha", new java.sql.Date(Compra.getStrFechaCompra().getTime()));
+            statement.setString("psubtotal", Compra.getStrSubTotalCompra());
+            statement.setString("pigv", Compra.getStrIgvCompra());
+            statement.setString("ptotal", Compra.getStrTotalCompra());
+            statement.setString("pestado", Compra.getStrEstadoCompra());
+            statement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        JOptionPane.showMessageDialog(null, "¡Compra Actualizada!", "Mensaje del Sistema", 1);
+    }
+
+    public ArrayList<ClsEntidadCompra> listarCompra() {
+        ArrayList<ClsEntidadCompra> compras = new ArrayList<ClsEntidadCompra>();
+        try {
+            CallableStatement statement = connection.prepareCall("{call SP_S_Compra}");
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                ClsEntidadCompra compra = new ClsEntidadCompra();
                 compra.setStrIdCompra(resultSet.getString("IdCompra"));
                 compra.setStrTipoDocumento(resultSet.getString("TipoDocumento"));
                 compra.setStrProveedor(resultSet.getString("Proveedor"));
-                compra.setStrEmpleado(resultSet.getString("Empleado"));               
+                compra.setStrEmpleado(resultSet.getString("Empleado"));
                 compra.setStrNumeroCompra(resultSet.getString("Numero"));
                 compra.setStrFechaCompra(resultSet.getDate("Fecha"));
                 compra.setStrSubTotalCompra(resultSet.getString("SubTotal"));
@@ -82,71 +86,91 @@ private Connection connection=new ClsConexion().getConection();
                 compras.add(compra);
             }
             return compras;
-         }catch(SQLException ex){
+        } catch (SQLException ex) {
             ex.printStackTrace();
             return null;
         }
     }
-    public ResultSet listarCompraPorParametro(String criterio, String busqueda) throws Exception{
+
+    public ResultSet listarCompraPorParametro(String criterio, String busqueda) throws Exception {
         ResultSet rs = null;
-        try{
+        try {
             CallableStatement statement = connection.prepareCall("{call SP_S_CompraPorParametro(?,?)}");
             statement.setString("pcriterio", criterio);
             statement.setString("pbusqueda", busqueda);
             rs = statement.executeQuery();
             return rs;
-        }catch(SQLException SQLex){
-            throw SQLex;            
-        }        
-    }    
-    public ResultSet obtenerUltimoIdCompra() throws Exception{
+        } catch (SQLException SQLex) {
+            throw SQLex;
+        }
+    }
+
+    public ResultSet obtenerUltimoIdCompra() throws Exception {
         ResultSet rs = null;
-        try{
+        try {
             CallableStatement statement = connection.prepareCall("{call SP_S_UltimoIdCompra()}");
             rs = statement.executeQuery();
             return rs;
-        }catch(SQLException SQLex){
-            throw SQLex;            
-        }        
-    }      
-    
-    public ResultSet listarCompraPorFecha(String criterio,Date fechaini, Date fechafin, String doc) throws Exception{
+        } catch (SQLException SQLex) {
+            throw SQLex;
+        }
+    }
+
+    public ResultSet listarCompraPorFecha(String criterio, Date fechaini, Date fechafin, String doc) throws Exception {
         ResultSet rs = null;
-        try{
+        try {
             CallableStatement statement = connection.prepareCall("{call SP_S_CompraPorFecha(?,?,?,?)}");
-            statement.setString ("pcriterio", criterio);
-            statement.setDate ("pfechaini", new java.sql.Date(fechaini.getTime()));
-            statement.setDate ("pfechafin", new java.sql.Date(fechafin.getTime()));
+            statement.setString("pcriterio", criterio);
+            statement.setDate("pfechaini", new java.sql.Date(fechaini.getTime()));
+            statement.setDate("pfechafin", new java.sql.Date(fechafin.getTime()));
             statement.setString("pdocumento", doc);
             rs = statement.executeQuery();
             return rs;
-        }catch(SQLException SQLex){
-            throw SQLex;            
-        }        
-    } 
-    public void actualizarCompraEstado(String codigo,ClsEntidadCompra Compra){
-        try{
-            CallableStatement statement=connection.prepareCall("{call SP_U_ActualizarCompraEstado(?,?)}");
-            statement.setString("pidcompra",codigo);
-            statement.setString("pestado",Compra.getStrEstadoCompra());        
+        } catch (SQLException SQLex) {
+            throw SQLex;
+        }
+    }
+
+    public void actualizarCompraEstado(String codigo, ClsEntidadCompra Compra) {
+        try {
+            CallableStatement statement = connection.prepareCall("{call SP_U_ActualizarCompraEstado(?,?)}");
+            statement.setString("pidcompra", codigo);
+            statement.setString("pestado", Compra.getStrEstadoCompra());
             statement.executeUpdate();
-            
-        }catch(SQLException ex){
+
+        } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        JOptionPane.showMessageDialog(null,"¡Compra Anulada!","Mensaje del Sistema",1);
+        JOptionPane.showMessageDialog(null, "¡Compra Anulada!", "Mensaje del Sistema", 1);
     }
-    public ResultSet listarCompraPorDetalle(String criterio,Date fechaini, Date fechafin) throws Exception{
+
+    public ResultSet listarCompraPorDetalle(String criterio, Date fechaini, Date fechafin) throws Exception {
         ResultSet rs = null;
-        try{
+        try {
             CallableStatement statement = connection.prepareCall("{call SP_S_CompraPorDetalle(?,?,?)}");
-            statement.setString ("pcriterio", criterio);
-            statement.setDate ("pfechaini", new java.sql.Date(fechaini.getTime()));
-            statement.setDate ("pfechafin", new java.sql.Date(fechafin.getTime()));
+            statement.setString("pcriterio", criterio);
+            statement.setDate("pfechaini", new java.sql.Date(fechaini.getTime()));
+            statement.setDate("pfechafin", new java.sql.Date(fechafin.getTime()));
             rs = statement.executeQuery();
             return rs;
-        }catch(SQLException SQLex){
-            throw SQLex;            
-        }        
+        } catch (SQLException SQLex) {
+            throw SQLex;
+        }
+    }
+
+    public boolean agregarlote(String codigo, String fechacaducidad, int cantidad, int Idproducto) {
+        CallableStatement call = null;
+        try {
+            call = connection.prepareCall("{call guardarlote(?,?,?,?)}");
+            call.setString("codig", codigo);
+            call.setString("fecha_Caducidad", fechacaducidad);
+            call.setInt("cantidadcomprado", cantidad);
+            call.setInt("Idproducto", Idproducto);
+            trueandfalselote=call.execute();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se a podido guardar el lote");
+        }
+        return trueandfalselote;
     }
 }
